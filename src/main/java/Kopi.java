df709b6132c3ce20b038c073ae3f521c7d57cdd4
Ding Bowen <dingbowen2002@hotmail.com>
2026-02-04 01:18:52 +0800

    Level-0: Rename to KopiOKosongPeng

---
+++ src/main/java/Kopi.java
@@ -0,0 +1,17 @@
+public class Kopi {
+    public static void main(String[] args) {
+        String logo = " _  __  ___   ____   ___ \n"
+                + "| |/ / / _ \\ |  _ \\ |_ _|\n"
+                + "| ' / | | | || |_) | | | \n"
+                + "| . \\ | |_| ||  __/  | | \n"
+                + "|_|\\_\\ \\___/ |_|    |___|\n";
+
+        String bar = "------------------------------------------------------\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println(bar);
+        System.out.println("Hello! I'm KopiOKosongPeng");
+        System.out.println("What can I do for you?\n" + bar);
+
+        System.out.println("Bye. Hope to see you again soon!\n" + bar);
+    }
+}
