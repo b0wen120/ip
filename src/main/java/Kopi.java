e29891c05d25ebe9bb38aff11d038955e9e2b926
Ding Bowen <dingbowen2002@hotmail.com>
2026-02-05 02:00:18 +0800

    Level-3: Mark as Done

--- src/main/java/Kopi.java
+++ src/main/java/Kopi.java
@@ -4,7 +4,7 @@ public class Kopi {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        String[] allTasks = new String[100];
+        Task[] allTasks = new Task[100];
         int allTasksCount = 0;
 
         String logo = " _  __  ___   ____   ___ \n"
@@ -20,16 +20,18 @@ public class Kopi {
         System.out.println("What you want?\n" + bar);
 
         while (true) {
-            String input = in.nextLine();
+            String userInput = in.nextLine().trim();
 
             System.out.println(bar);
 
-            if (input.equals("bye")) {
+            if (userInput.equals("bye")) {
                 System.out.println("Bye bye, come next order!\n" + bar);
                 break;
             }
 
-            if (input.equals("list")) {
+            if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:\n");
+
                 for (int i = 0; i < allTasksCount; i++) {
                     System.out.println((i + 1) + ". " + allTasks[i]);
                 }
@@ -37,10 +39,33 @@ public class Kopi {
                 continue;
             }
 
-            allTasks[allTasksCount] = input;
+            if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) -1;
+
+                if (taskIndex > 0 && taskIndex < allTasksCount) {
+                    allTasks[taskIndex].markAsDone();
+                    System.out.println("Nice lah this one finish liao:");
+                    System.out.println(" " + allTasks[taskIndex]);
+                }
+                System.out.println(bar);
+                continue;
+            }
+
+            if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) -1;
+
+                if (taskIndex > 0 && taskIndex < allTasksCount) {
+                    allTasks[taskIndex].unmarkAsNotDone();
+                    System.out.println("Orh okay, so this one haven't finish:");
+                    System.out.println(" " + allTasks[taskIndex]);
+                }
+                System.out.println(bar);
+                continue;
+            }
+            allTasks[allTasksCount] = new Task(userInput);
             allTasksCount++;
 
-            System.out.println("added: " + input);
+            System.out.println("added: " + userInput);
             System.out.println(bar);
         }
     }
